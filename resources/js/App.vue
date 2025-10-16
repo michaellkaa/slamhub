e5f7f72053646f0aeb9eea4df3a830fe7f4b9a89
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-10-16 07:43:20 +0200

    barvicka mi nejde a pracuju na navigaci

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="bg-[#1C1C1C] w-screen h-screen">
+    <div class="bg-[#1C1C1C] w-screen h-screen flex flex-row">
+        <div class="flex flex-col h-screen">
+        <SideNav/>
+
+        </div>
+        <div class="">
         <h2 class="text-red-700 bg-blue-400">
         VueSchool Rules!
         </h2>
         <Test/>
+        </div>
+
     </div>
 </template>
 
 <script setup>
 import Test from './components/Test.vue'
+import SideNav from './components/SideNav.vue';
 </script>
--- resources/js/components/SideNav.vue
+++ resources/js/components/SideNav.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="bg-[#1E1E1E]">
-        <img src="../../../public/home.svg" alt="">
+    <div class="bg-[#1E1E1E] h-screen w-28">
+        <div class="flex flex-col justify-center fill-white">
+        <img src="../../../public/home.svg" alt="" class="fill-white">
+        <img src="../../../public/calendar.svg" alt="">
+        <img src="../../../public/award.svg" alt="">
+        <img src="../../../public/send.svg" alt="">
+        <img src="../../../public/user.svg" alt="">
+        </div>
     </div>
 </template>
