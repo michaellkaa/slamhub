f2741be0d2a3043ee3dd269217251421e14cb586
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-11-12 12:05:45 +0100

    update

--- resources/js/pages/AwardPages.vue
+++ resources/js/pages/AwardPages.vue
@@ -1,34 +1,38 @@
 <template>
-    <div class="bg-[#FFF7CC] w-screen h-screen flex overflow-hidden"> 
-        <div class="h-full rounded-3xl z-10 shadow-[4px_0_15px_rgba(0,0,0,0.4)]">
-            <SideNav :activeNav="activeNav" @navigate="handleNavigate" />
+    <div class="bg-[#FFF7CC] w-screen h-screen flex flex-col lg:flex-row overflow-hidden">
+      <!-- SideNav -->
+      <div class="lg:h-full lg:w-28 w-full h-36 fixed bottom-0 lg:static z-10">
+        <SideNav :activeNav="activeNav" @navigate="handleNavigate" />
+      </div>
+  
+      <!-- Obsah stránky -->
+      <div class="flex-1 flex flex-col mt-4 lg:mt-28 overflow-y-auto pb-28 lg:pb-0">
+        <div class="grid grid-cols-2 gap-4 w-full px-4">
+          <SwitchTitle text="Liga" :isActive="activeTab === 'league'" @click="activeTab = 'league'" />
+          <SwitchTitle text="Žebříček" :isActive="activeTab === 'leaderboard'" @click="activeTab = 'leaderboard'" />
         </div>
-        <div class="flex-1 flex flex-col mt-28 overflow-y-auto"> 
-            <div class="grid grid-cols-2 gap-4 w-full px-4">
-                <SwitchTitle text="Liga" :isActive="activeTab === 'league'" @click="activeTab = 'league'" />
-                <SwitchTitle text="Žebříček" :isActive="activeTab === 'leaderboard'"
-                    @click="activeTab = 'leaderboard'" />
-            </div> 
-            <div class="mt-4 w-full">
-                <League v-if="activeTab === 'league'" />
-                <Leaderboard v-if="activeTab === 'leaderboard'" />
-            </div>
+  
+        <div class="mt-4 w-full">
+          <League v-if="activeTab === 'league'" />
+          <Leaderboard v-if="activeTab === 'leaderboard'" />
         </div>
+      </div>
     </div>
-</template>
-
-<script setup>    
-import SideNav from '../components/SideNav.vue' 
-import Leaderboard from '../components/Leaderboard.vue' 
-import League from '../components/League.vue' 
-import SwitchTitle from '../components/SwitchTitle.vue' 
-
-import { ref } from 'vue' 
-const activeTab = ref('leaderboard') 
-const activeNav = ref('award') 
-
-
-const handleNavigate = (nav) => {
-  activeNav.value = nav;
-};
-</script>
+  </template>
+  
+  <script setup>
+  import SideNav from '../components/SideNav.vue'
+  import Leaderboard from '../components/Leaderboard.vue'
+  import League from '../components/League.vue'
+  import SwitchTitle from '../components/SwitchTitle.vue'
+  
+  import { ref } from 'vue'
+  
+  const activeTab = ref('leaderboard')
+  const activeNav = ref('award')
+  
+  const handleNavigate = (nav) => {
+    activeNav.value = nav
+  }
+  </script>
+  
