4f1058e9a47cab0999dde23f2063cc20f2097c5b
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2026-04-17 10:11:21 +0200

    try awards

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Award extends Model
 {
@@ -16,6 +17,20 @@ class Award extends Model
         'created_by',
     ];
 
+    protected $appends = ['icon_url'];
+
+    public function getIconUrlAttribute()
+    {
+        if (!$this->icon_path) {
+            return asset('images/default-award.png'); 
+        }
+
+        if (filter_var($this->icon_path, FILTER_VALIDATE_URL)) {
+            return $this->icon_path;
+        }
+
+        return asset('storage/' . $this->icon_path);
+    }
 
     public function creator()
     {
